1488f2e8a29d2f4851c551bd730758da2b45feba
Cagdas Ucar <cucar@abacuslaw.com>
2019-06-05 06:56:04 -0700

    catalog mobile view styling

--- client/src/pages/catalog/catalogProduct.js
+++ client/src/pages/catalog/catalogProduct.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import LinkButton from '../../shared/linkButton';
 import { Card, CardContent } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
 
 import './catalogProduct.css';
 
@@ -18,7 +17,7 @@ export default function CatalogProduct({ product }) {
 					</div>
 					<div className="catalog-product-info">
 						<div className="catalog-product-header">
-							<Typography variant="h4">{product.name}</Typography>
+							{product.name}
 						</div>
 						<div className="catalog-product-text">
 							{product.description}
@@ -26,9 +25,8 @@ export default function CatalogProduct({ product }) {
 					</div>
 					<div className="catalog-product-price">
 						<div>
-							<Typography variant="h6">${parseFloat(product.discounted_price) > 0 ? product.discounted_price : product.price}</Typography>
-							<br/>
-							{parseFloat(product.discounted_price) > 0 && <span className="discounted-from">${product.price}</span>}
+							<span className="catalog-product-effective-price">${parseFloat(product.discounted_price) > 0 ? product.discounted_price : product.price}</span>
+							{parseFloat(product.discounted_price) > 0 && <span className="catalog-product-discounted-from">${product.price}</span>}
 						</div>
 					</div>
 					<div className="catalog-product-link">
