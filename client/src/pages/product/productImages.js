e7f1654d0787161d144a8a111235e9350d11e897
Cagdas Ucar <cucar@abacuslaw.com>
2019-06-07 11:56:18 -0700

    remove gallery height setting - it looks better when it's dynamic actually - mobile styling works as well

--- client/src/pages/product/productImages.js
+++ client/src/pages/product/productImages.js
@@ -14,23 +14,6 @@ export default function ProductImages({ images }) {
 		thumbnail: `/Images/product_images/${image}`
 	}));
 	
-	/**
-	 * image gallery height is dynamic by default - our image heights are different, so it causes fluctuations of height when you go to next image
-	 * we remedy that here by tapping into on image load function of the gallery and determining the image with the max height and setting the gallery height to that
-	 */
-	let maxImageHeight = 0;
-	const onImageLoad = (event) => {
-		
-		// at the initial image load, there is nothing to do - just set the first image height and move on - the gallery height is already the first image height
-		if (maxImageHeight === 0) { maxImageHeight = event.target.height; return; }
-
-		// if we come across an image that was greater than anything we saw before, record it and set the gallery height to new high
-		if (event.target.height > maxImageHeight) {
-			maxImageHeight = event.target.height;
-			document.getElementsByClassName('image-gallery-slide-wrapper')[0].style.height = maxImageHeight + 'px';
-		}
-	};
-	
 	return (
 		<ImageGallery
 			items={imageGalleryImages}
@@ -39,7 +22,6 @@ export default function ProductImages({ images }) {
 			showPlayButton={false}
 			showIndex={false}
 			showNav={false}
-			onImageLoad={onImageLoad}
 		/>
 	);
 }
